5e40d263323f87160fe1246901617b3f67880d77
Daniel Supernault <danielsupernault@gmail.com>
2016-07-02 20:24:14 -0600

    Update user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name', 40)->nullable();
+            $table->string('username', 15)->unique()->index();
             $table->string('email')->unique();
             $table->string('password');
+            $table->tinyInteger('role')->unsigned()->default(0);
+            $table->string('ip')->nullable();
             $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
     }
