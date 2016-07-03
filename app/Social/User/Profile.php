9ccd7884bbbba55622a063eb6e5662e3117ef620
Daniel Supernault <danielsupernault@gmail.com>
2016-07-02 20:30:04 -0600

    Add Profile Model/Migration

---
+++ app/Social/User/Profile.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Social\User;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    //
+}
---
+++ database/migrations/2016_07_03_022535_create_profile_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('bio', 140)->nullable();
+            $table->string('homepage')->nullable();
+            $table->string('sex')->nullable();
+            $table->string('location')->nullable();
+            $table->timestamp('birthdate')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('profile');
+    }
+}
