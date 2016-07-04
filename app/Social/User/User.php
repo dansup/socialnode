71e05193bf285d431c078d08ba3ae383c61ce49e
Daniel Supernault <danielsupernault@gmail.com>
2016-07-03 18:08:39 -0600

    Add new methods to User model

--- app/Social/User/User.php
+++ app/Social/User/User.php
@@ -23,4 +23,24 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function statuses()
+    {
+        return $this->hasMany('App\Social\Graph\Status');
+    }
+
+    public function profile()
+    {
+        return $this->hasOne('App\Social\User\Profile', 'user_id', 'id');
+    }
+
+    public function followers()
+    {
+        return $this->hasMany('App\Social\Graph\Follower', 'followed_id', 'id');
+    }
+
+    public function following()
+    {
+        return $this->hasMany('App\Social\Graph\Follower', 'follower_id', 'id');
+    }
 }
