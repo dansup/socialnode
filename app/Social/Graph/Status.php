369c961b6b149ea94c0f50dc90a73b1b4e157298
Daniel Supernault <danielsupernault@gmail.com>
2016-07-03 18:06:43 -0600

    Move status model

---
+++ app/Social/Graph/Status.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Social\Graph;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Status extends Model
+{
+    protected $table = 'status';
+
+    public function buildUrl()
+    {
+      $id = $this->id;
+      $username = $this->user->username;
+    }
+
+    public function user()
+    {
+      return $this->belongsTo('App\Social\User\User');
+    }
+}
