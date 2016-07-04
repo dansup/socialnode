86ffe4a5add555fa14a31164796337c8582a872a
Daniel Supernault <danielsupernault@gmail.com>
2016-07-03 18:10:08 -0600

    Add likes model/migration

---
+++ app/Social/Graph/Like.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Social\Graph;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{
+    protected $table = "likes";
+
+}
---
+++ database/migrations/2016_07_03_234953_create_likes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('object_id')->unsigned();
+            $table->string('object_type')->nullable();
+            $table->bigInteger('user_id')->unsigned();
+            $table->unique(['object_id', 'object_type', 'user_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('likes');
+    }
+}
